2c6d288713569ad95e5f8566bc9d45d70f12bae1
SATISH REDDY SATTI <satishdevops3789@gmail.com>
2024-09-26 18:10:10 +0530

    Create node_repo.sh

---
+++ node_repo.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+sudo apt update
+sudo apt install curl
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y nodejs
+
+#INSTALLING USING NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+source ~/.bashrc
+nvm install node
+nvm install node
+#npm install -g npm@latest #Update npm with latest version
+
