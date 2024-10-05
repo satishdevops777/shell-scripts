0361ad35b76b0f591e18456fb0e981ee7877b88b
SATISH REDDY SATTI <satishdevops3789@gmail.com>
2024-10-05 13:42:07 +0530

    Update 7.Promethus_node_exporter.sh

--- 7.Promethus_node_exporter.sh
+++ 7.Promethus_node_exporter.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 VERSION="1.8.2"
-
+USER="prometheus"
 wget https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.linux-amd64.tar.gz
 tar -xzf node_exporter-${VERSION}.linux-amd64.tar.gz
 mv node_exporter-${VERSION}.linux-amd64 node_exporter
 cd node_exporter
 sudo cp node_exporter /usr/local/bin/
-
+sudo chown ${USER}:${USER} /usr/local/bin/node_exporter
 cat <<EOL | sudo tee /etc/systemd/system/node_exporter.service
 [Unit]
 Description=Node Exporter
 
 [Service]
-User=nobody
+User=prometheus
 ExecStart=/usr/local/bin/node_exporter
 
 [Install]
