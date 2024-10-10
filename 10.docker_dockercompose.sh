789c6fbe39fbd92871b829ba35b3c5b1c64f2064
SATISH REDDY SATTI <satishdevops3789@gmail.com>
2024-10-10 12:21:06 +0530

    Update 10.docker_dockercompose.sh

--- 10.docker_dockercompose.sh
+++ 10.docker_dockercompose.sh
@@ -14,7 +14,7 @@ echo \
 sudo apt-get update
 
 # Install the Docker packages.
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
